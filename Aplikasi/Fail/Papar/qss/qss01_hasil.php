b4cfc4039c5fe090d88bd8cf2951ffab9e7505f1
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-10-25 16:58:40 +0800

    set pembolehubah jika wujud

--- Aplikasi/Fail/Papar/qss/qss01_hasil.php
+++ Aplikasi/Fail/Papar/qss/qss01_hasil.php
@@ -25,13 +25,14 @@ include 'diatas-am.php';
 	<script  language='JavaScript' type='text/javascript' src='<?php echo $pautan?>js_file/js/validasi/a_internaluser.js'></SCRIPT>	
 </div>
 */
+$sektor = (isset($this->_jadual)) ? $this->_jadual : null;
 $link[0]= URL . 'qss';
 $link[1]= 'https://qss.stats.gov.my/qssv2/pdf/pdf3.php?no_siri='
 . $this->dataID . '&sukutahun=' . $this->suku .'&tahun=2018';
 $link[2] = 'https://qss.stats.gov.my/qssv2/pagemenu.php?pref='
 . $this->suku . '&getSerial=' . $this->dataID . '';
 ?>
-<br>Sektor : <?php echo $this->_jadual ?> | <a href="<?php echo $link[0] ?>">Anjung</a>
+<br>Sektor : <?php echo $sektor ?> | <a href="<?php echo $link[0] ?>">Anjung</a>
 | <a target="_blank" href="<?php echo $link[1] ?>"><?php echo 'Suku 1'//$link[1] ?></a>
 | Suku 2 : <a target="_blank" href="<?php echo $link[2] ?>"><?php echo $link[2] ?></a>
 <!-- isi borang mula ########################################################################################################## -->
