f5a7d4c55fd0d23aaecb59a3f532217c458f2374
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-10-25 17:16:47 +0800

    buang <br>

--- Aplikasi/Fail/Papar/qss/qss01_hasil.php
+++ Aplikasi/Fail/Papar/qss/qss01_hasil.php
@@ -30,9 +30,8 @@ $link[0]= URL . 'qss';
 $link[1]= 'https://qss.stats.gov.my/qssv2/pdf/pdf3.php?no_siri='
 . $this->dataID . '&sukutahun=' . $this->suku .'&tahun=2018';
 $link[2] = 'https://qss.stats.gov.my/qssv2/pagemenu.php?pref='
-. $this->suku . '&getSerial=' . $this->dataID . '';
-?>
-<br>Sektor : <?php echo $sektor ?> | <a href="<?php echo $link[0] ?>">Anjung</a>
+. $this->suku . '&getSerial=' . $this->dataID . ''; ?>
+Sektor : <?php echo $sektor ?> | <a href="<?php echo $link[0] ?>">Anjung</a>
 | <a target="_blank" href="<?php echo $link[1] ?>"><?php echo 'PDF'//$link[1] ?></a>
 | <a target="_blank" href="<?php echo $link[2] ?>"><?php echo 'Suku 3'//$link[2] ?></a>
 <!-- isi borang mula ########################################################################################################## -->
