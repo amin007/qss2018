8080f98608cca107a2d18c1093e63198907fdd85
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-10-25 12:22:17 +0800

    tukar tajuk atas

--- Aplikasi/Fail/Papar/qss/diatas-am.php
+++ Aplikasi/Fail/Papar/qss/diatas-am.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
 <meta name="author" content="">
-<title>E-QSS</title>
+<title>E-QSS - POM</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link href="<?=FONTAWESOME510?>" rel="stylesheet" type="text/css">
 <link href="<?=BOOTSTRAPCSS411?>" rel="stylesheet" type="text/css">
