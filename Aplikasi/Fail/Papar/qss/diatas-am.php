2dc64a15491464974c679c5d3a47f875b3398c4f
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-10-25 12:43:10 +0800

    tambah css

--- Aplikasi/Fail/Papar/qss/diatas-am.php
+++ Aplikasi/Fail/Papar/qss/diatas-am.php
@@ -9,3 +9,21 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link href="<?=FONTAWESOME510?>" rel="stylesheet" type="text/css">
 <link href="<?=BOOTSTRAPCSS411?>" rel="stylesheet" type="text/css">
+<style>
+.textdescrp{
+font-size:14px;
+font-weight:bold;
+font-family:"Helvetica Neue", Arial, "Liberation Sans", FreeSans, sans-serif;
+}
+.textdescrp1{
+/*font-size:13px;*/
+font-size:8px;
+font-weight:bold;
+font-family:"Helvetica Neue", Arial, "Liberation Sans", FreeSans, sans-serif;
+}
+.textdescrp2{
+/*font-size:13px;*/
+font-size:8px;
+font-family:"Helvetica Neue", Arial, "Liberation Sans", FreeSans, sans-serif;
+}
+</style>
