7c61b06a74f35c46bbfb0481e4c60dd6f21bdef0
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-10-28 18:36:40 +0800

    tambah css excel

--- Aplikasi/Fail/Papar/qss/diatas-am.php
+++ Aplikasi/Fail/Papar/qss/diatas-am.php
@@ -27,3 +27,27 @@ font-size:8px;
 font-family:"Helvetica Neue", Arial, "Liberation Sans", FreeSans, sans-serif;
 }
 </style>
+<style type="text/css">
+table.excel {
+	border-style:ridge;
+	border-width:1;
+	border-collapse:collapse;
+	font-family:sans-serif;
+	font-size:11px;
+}
+table.excel thead th, table.excel tbody th {
+	background:#CCCCCC;
+	border-style:ridge;
+	border-width:1;
+	text-align: center;
+	vertical-align: top;
+}
+table.excel tbody th { text-align:center; vertical-align: top; }
+table.excel tbody td { vertical-align:bottom; }
+table.excel tbody td
+{
+	padding: 0 3px; border: 1px solid #aaaaaa;
+	background:#ffffff;
+}
+.fa-input {font-family: FontAwesome}
+</style>
