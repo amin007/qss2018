f531227717203f025e6bc17f72d96332237e92d4
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-10-30 17:19:03 +0800

    tambah pautan

--- Aplikasi/Fail/Papar/qss/qss01_hasil.php
+++ Aplikasi/Fail/Papar/qss/qss01_hasil.php
@@ -31,10 +31,12 @@ $link[0]= URL . 'qss';
 $link[1]= 'https://qss.stats.gov.my/qssv2/pdf/pdf3.php?no_siri='
 . $this->dataID . '&sukutahun=' . $this->suku .'&tahun=2018';
 $link[2] = 'https://qss.stats.gov.my/qssv2/pagemenu.php?pref='
-. $this->suku . '&getSerial=' . $this->dataID . ''; ?>
+. $this->suku . '&getSerial=' . $this->dataID . '';
+$link[3] = URL . 'qss/suku' . $this->suku . '/' . $this->dataID . '/' . $this->suku . '/edagang' ?>
 Sektor : <?php echo $sektor ?> | <a href="<?php echo $link[0] ?>">Anjung</a>
-| <a target="_blank" href="<?php echo $link[1] ?>"><?php echo 'PDF'//$link[1] ?></a>
-| <a target="_blank" href="<?php echo $link[2] ?>"><?php echo 'Suku 3'//$link[2] ?></a>
+| <a target="_blank" href="<?php echo $link[1] ?>"><?php echo 'PDF' ?></a>
+| <a target="_blank" href="<?php echo $link[2] ?>"><?php echo 'Suku 3' ?></a>
+| <a target="_blank" href="<?php echo $link[3] ?>"><?php echo 'Edagang' ?></a>
 <!-- isi borang mula ########################################################################################################## -->
 <?php
 include 'kiraFungsi.php';
