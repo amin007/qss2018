9fcb9b7d5889a788a10ace60b5c09c98eba0e3de
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-28 10:50:24 +0800

    tambah fungsi js

--- Aplikasi/Fail/Papar/ejob/kira-ot.php
+++ Aplikasi/Fail/Papar/ejob/kira-ot.php
@@ -21,46 +21,51 @@ Contoh
 <form class="container">
 <div class="form-row">
 	<div class="form-group col-md-6">
-		<label for="inputEmail4">Email</label>
-		<input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+		<label for="inputGaji">Gaji</label>
+		<input type="email" class="form-control" id="gaji" placeholder="Gaji">
 	</div>
 	<div class="form-group col-md-6">
-		<label for="inputPassword4">Password</label>
-		<input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+		<label for="inputBulan">Bulan</label>
+		<input type="password" class="form-control" id="bulan" value="12" placeholder="12">
 	</div>
 </div>
-<div class="form-group">
-	<label for="inputAddress">Address</label>
-	<input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-</div>
-<div class="form-group">
-	<label for="inputAddress2">Address 2</label>
-	<input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-</div>
-<div class="form-row">
-	<div class="form-group col-md-6">
-		<label for="inputCity">City</label>
-		<input type="text" class="form-control" id="inputCity">
-	</div>
-	<div class="form-group col-md-4">
-		<label for="inputState">State</label>
-		<select id="inputState" class="form-control">
-		<option selected>Choose...</option>
-		<option>...</option>
-		</select>
-	</div>
-	<div class="form-group col-md-2">
-		<label for="inputZip">Zip</label>
-		<input type="text" class="form-control" id="inputZip">
-	</div>
-</div>
-<div class="form-group">
-	<div class="form-check">
-		<input class="form-check-input" type="checkbox" id="gridCheck">
-		<label class="form-check-label" for="gridCheck">Check me out</label>
-	</div>
-</div>
-	<button type="submit" class="btn btn-primary">Sign in</button>
 </form>
 
-<?php include 'atas/dibawah.php'; ?>
+<?php echo "\n"; ?><!-- Footer
+================================================== -->
+<!-- footer class="footer">
+	<div class="container">
+		<span class="label label-info">
+		&copy; Hak Cipta Terperihara 2016. Theme <?php
+$theme = ( !isset($pilih) ) ? 'Asal Bootstrap Twitter' : $pilih;
+echo $theme = (isset($theme)) ? $theme : null; ?>
+		</span>
+	</div>
+</footer -->
+<!-- khas untuk jquery dan js2 lain
+================================================== -->
+<script type="text/javascript" src="<?=JQUERY?>"></script>
+<script type="text/javascript" src="<?=BOOTSTRAPJS411?>"></script>
+<?php
+if (isset($this->js))
+{
+    foreach ($this->js as $js)
+    {
+        echo "\n";
+		?><script type="text/javascript" src="<?php echo $js ?>"></script><?php
+    }
+}
+?>
+<script>
+    $('#texttwo').keyup(function(){
+        var textone;
+        var texttwo;
+        textone = parseFloat($('#textone').val());
+        texttwo = parseFloat($('#texttwo').val());
+        var result = textone + texttwo;
+        $('#result').val(result.toFixed(0));
+    });
+</script>
+
+</body>
+</html>
